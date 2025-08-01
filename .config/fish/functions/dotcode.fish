e474437cee981d99a48eb4a48cede7df23f96a06
noisegrrrl <noisegrrrl@proton.me>
2025-08-01 13:52:31 +0200

    cleanup + kitty theme + fish functions

---
+++ .config/fish/functions/dotcode.fish
@@ -0,0 +1,4 @@
+function dotcode --wraps='GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME"' --description 'alias dotcode=GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME"'
+  GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME" $argv
+        
+end
