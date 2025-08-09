65f0e47a00722ef946e64477e914fb04c750d2fa
noisegrrrl <13435869+noisegrrrl@users.noreply.github.com>
2025-08-09 19:36:53 +0200

    add codium conf

--- .config/fish/functions/dotcode.fish
+++ .config/fish/functions/dotcode.fish
@@ -1,3 +1,3 @@
 function dotcode --wraps='GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME"' --description 'alias dotcode=GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME"'
-  GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" code "$HOME" $argv
+  GIT_WORK_TREE="$HOME" GIT_DIR="$HOME/.dotfiles" vscodium "$HOME" --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland $argv
 end
