941d3d4a84b61006733c301f26de46aadce654fe
noisegrrrl <13435869+noisegrrrl@users.noreply.github.com>
2026-02-03 15:36:22 +0100

    waybar + nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,11 +5,6 @@ require 'lsp'
 
 require 'telescope-config'
 require 'chezmoi-config'
--- disable netrw at the very start of your init.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
 
-vim.g.tidal_target = "terminal"
-vim.g.vim_niri_nav_workspace = "true"
 -- vim.cmd("colorscheme root-loops")
 vim.cmd("colorscheme ashen")
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,16 +1,22 @@
+-- neo-tree.nvim
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range('3')
+  },
+  -- dependencies
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  -- optional, but recommended
+  "https://github.com/nvim-tree/nvim-web-devicons",
+})
+
 vim.pack.add {
-    { src = 'https://github.com/nvim-lua/plenary.nvim' },
-    { src = 'https://github.com/MunifTanjim/nui.nvim' },
-    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-    { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim' },
     { src = 'https://github.com/imsnif/kdl.vim' },
-    -- { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
-    -- { src = 'https://github.com/stevearc/oil.nvim' },
-    { src = 'https://github.com/tidalcycles/vim-tidal' },
     { src = 'https://github.com/xvzc/chezmoi.nvim' },
-    { src = 'https://github.com/noisegrrrl/vim-niri-nav' },
     { src = 'https://codeberg.org/ficd/ashen.nvim' }
 }
--- require("oil").setup()
+
+
