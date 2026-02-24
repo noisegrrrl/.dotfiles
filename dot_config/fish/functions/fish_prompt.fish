3a314f483a637b0917fe523531afe41078ce6e16
noisegrrrl <13435869+noisegrrrl@users.noreply.github.com>
2026-02-24 21:20:31 +0100

    fish prompt

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -5,16 +5,17 @@ function fish_prompt --description 'Informative prompt'
 
     if functions -q fish_is_root_user; and fish_is_root_user
         printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-                                                                     and set_color $fish_color_cwd_root
-                                                                     or set_color $fish_color_cwd) \
+        and set_color $fish_color_cwd_root
+        or set_color $fish_color_cwd) \
             (prompt_pwd) (set_color normal)
     else
         set -l status_color (set_color $fish_color_status)
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '%s%s %s %s%s %s%s%s \n> ' (set_color brblue) \
-            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-            (set_color normal)
+        printf '%s %s%s %s\n%s>%s ' \
+            (prompt_login) \
+            (set_color $fish_color_cwd)  (prompt_pwd --full-length-dirs 2) \
+            $pipestatus_string (set_color --bold normal) (set_color normal) 
     end
 end
